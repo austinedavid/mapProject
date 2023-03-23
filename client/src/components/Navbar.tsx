d6debd5553aea9ff75a30edab1f3306f01156161
david <austinedavid96@gmail.com>
2023-03-23 15:51:51 +0100

    checking why its not running on netlify

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import '../styles/Navbar.css'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import '../styles/Sidebar.css'
 import {cities, Icity} from "../utils/Cities"
 
